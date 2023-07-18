5e9297e9c83bedfbf5189490fc802eb8019ccf22
DimaDidenko1 <didenkdmytr@gmail.com>
2023-07-19 02:18:34 +0300

    2

--- js/main.js
+++ js/main.js
@@ -3,42 +3,42 @@ let content = document.querySelector('.content');
 let isBurgerMenuOpen = false;
 
 
-// function timerInPage() {
-//   var hoursElement = document.getElementById("Hours");
-//   var minsElement = document.getElementById("Mins");
-//   var secsElement = document.getElementById("Secs");
+function timerInPage() {
+  var hoursElement = document.getElementById("Hours");
+  var minsElement = document.getElementById("Mins");
+  var secsElement = document.getElementById("Secs");
   
   
-//   var hoursText = hoursElement.textContent;
-//   var minsText = minsElement.textContent;
-//   var secsText = secsElement ? secsElement.textContent : "";
+  var hoursText = hoursElement.textContent;
+  var minsText = minsElement.textContent;
+  var secsText = secsElement ? secsElement.textContent : "";
   
-//   var hours = parseInt(hoursText);
-//   var mins = parseInt(minsText);
-//   var secs = parseInt(secsText);
+  var hours = parseInt(hoursText);
+  var mins = parseInt(minsText);
+  var secs = parseInt(secsText);
 
-//   if(secs <= 0){
-//     if(mins <=0){
-//       if(hours){
+  if(secs <= 0){
+    if(mins <=0){
+      if(hours){
 
-//       }
-//       hours -= 1;
-//       mins = 59;
-//       hoursElement.innerText = hours.toString();
-//       minsElement.innerText = mins.toString();
-//     }
-//     else{
-//       mins -= 1;
-//       secs = 59;
-//       minsElement.innerText = mins.toString();
-//       secsElement.innerText = secs.toString();
-//     }
-//   }
-//   else{
-//     secs -= 1;
-//     secsElement.innerText = secs.toString();
-//   }
-// }
+      }
+      hours -= 1;
+      mins = 59;
+      hoursElement.innerText = hours.toString();
+      minsElement.innerText = mins.toString();
+    }
+    else{
+      mins -= 1;
+      secs = 59;
+      minsElement.innerText = mins.toString();
+      secsElement.innerText = secs.toString();
+    }
+  }
+  else{
+    secs -= 1;
+    secsElement.innerText = secs.toString();
+  }
+}
 
 // setInterval(timerInPage, 1000);
 
